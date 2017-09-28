f35f8aa79c9b0e0e13ea7af962e2ed0fac3fae4b
phra <greensoncio@gmail.com>
2017-09-28 12:07:33 +0200

    init: first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'API',
+      script    : 'app.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : 'localhost',
+      ref  : 'origin/master',
+      repo : 'git@github.com:phra/pm2-deploy-example.git',
+      path : '/tmp',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : 'localhost',
+      ref  : 'origin/master',
+      repo : 'git@github.com:phra/pm2-deploy-example.git',
+      path : '/tmp',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
